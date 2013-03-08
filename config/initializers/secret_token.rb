efc15ba6e83b2f39b61d841c83c00282809873ad
Keshav Prasad <keshavpra@gmail.com>
2013-03-08 15:09:08 +0530

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Toothsome::Application.config.secret_token = 'bd59d582f2a9269cc995bf1f9eb6006edda4d5bf67998b179b90b0799b44162a797385248058bb78af111967a003e22737f96729a3f5068991fe77208c5a4ad7'
