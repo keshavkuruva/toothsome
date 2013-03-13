9a7b9cdcc7b8f79a6afa8864a640c90a31b437ad
Arvind Naidu <disturbme@live.in>
2013-03-13 10:39:25 +0530

    Updated relation between client and product

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,4 @@
 class Client < ActiveRecord::Base
   attr_accessible :address_line1, :address_line2, :city, :country, :description, :email, :name, :phone_no, :postal_code, :state, :image_url
-  has_and_belongs_to_many :products
-  has_many :product_prices
+  has_many :products
 end
