82323dfcc460c1c1b65b8baa97b66b66db507f3c
sumanth45 <sumanth.nitw@gmail.com>
2013-03-21 14:47:05 +0530

    modified changes

--- db/migrate/20130313050613_add_client_id_to_products.rb
+++ db/migrate/20130313050613_add_client_id_to_products.rb
@@ -1,5 +1,5 @@
 class AddClientIdToProducts < ActiveRecord::Migration
   def change
-    add_column :products, :client_id, :integer, :limit => 11
+    add_column :products, :client_id, :integer
   end
 end
